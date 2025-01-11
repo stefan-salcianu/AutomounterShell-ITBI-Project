e76c24e9a34760af406bcb79f553d14ea6dcaedc
Neacsa Denis <neacsa.denis@yahoo.com>
2025-01-11 10:11:54 +0000

    inceput parte montare

--- asmh.sh
+++ asmh.sh
@@ -3,6 +3,7 @@
 run_shell() {
 	path="/home/Denis/Desktop/project_ITBI"
 	last_path="$path"
+	path_montare="$path/mountplace/"
 	while true; do
 		echo -n "ashm> "
 		read -r command
@@ -20,25 +21,39 @@ run_shell() {
                                 	echo "exista"
 					gasit="0"
 					echo "$gasit"
+					mount_cnt=1
 					while IFS=' ' read -r word0 word1 _; do
                                			 if [[ "$path/${command#cd }" == *"$word0"* ]]; then
+							mount_path="$word0"
 							gasit="1"
 							break
                                			 fi
+						let mount_cnt=mount_cnt+1
                         		done < /home/Denis/Desktop/project_ITBI/mountpoints 
+					path="$path/${command#cd }"
 					if [[ "$gasit" == "1" ]]; then
                               			 echo "mountpoint"
+						mount_info= $(mount | grep "$path")
+						if [[ -n "$mount_info" ]]; then
+							echo "montat"
+						else
+							echo "nemontat"
+							cd "/home/Denis/Desktop/project_ITBI"
+							cd "mountplace"
+							mkdir -p "mounted_$mount_cnt"
+							sudo mount "$path" "/home/Denis/Desktop/project_ITBI/mountplace/mounted_$mount_cnt" && echo "Montare reusita" || echo "Eroare"
+						fi
 						# gestionam situatia in care este mountpoint
                        			else
-						path="$path/${command#cd }"
                          			cd "$path"
                        				echo "nu este mountpoint" 
 					fi
 				 else
                             		echo "path invalid"
                        		 fi
-			fi
+
 			echo "$path"
+			fi
 		else
 			sh -c "$command"
 		fi
